27315681bbd4c83083d62babdfae36f652078a91
andreaoquendo <oquendo.3455@gmail.com>
2023-10-26 02:47:38 -0300

    no timer?

--- src/ppos-core-aux.c
+++ src/ppos-core-aux.c
@@ -455,7 +455,7 @@ int after_mqueue_msgs (mqueue_t *queue) {
 task_t * scheduler() {
     task_t* selectedTask = NULL;
 
-    // if(call_scheduler == 0){ return taskExec; }
+    if(call_scheduler == 0){ return taskExec; }
 
     // printf("\nENTROU NO SCHEDULER\n");
 
@@ -471,9 +471,11 @@ task_t * scheduler() {
             }
         } 
         
+        call_scheduler = 0;
         return selectedTask;
 
     }
+    call_scheduler = 0;
     return NULL;
    
 }
@@ -543,6 +545,10 @@ int task_getprio (task_t *task){
 }
 
 void tratador_timer(int signum){
+    call_scheduler = 1;
+    task_t* newTask = scheduler();
+    task_switch(newTask);
+    
     // taskExec->running_time+=1;
 
     // printf("\nquantum: %d\n", quantum);
